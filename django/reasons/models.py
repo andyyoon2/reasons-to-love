0c2a5bf66f17e9c76020c50d495050b6641d9080
Andy Yoon <insoo.yoon@proton.me>
2023-08-13 17:34:34 -0700

    Hookup postgres db and make migration, fix models

--- django/reasons/models.py
+++ django/reasons/models.py
@@ -2,25 +2,30 @@ from django.db import models
 from django.core.validators import RegexValidator
 
 # Create your models here.
-class User(models.model):
+class Partnership(models.Model):
+    """A pairing of 2 partners"""
+    name = models.CharField(max_length=100, null=True, blank=True)
+
+class User(models.Model):
+    """A user of the app, and a partner in a partnership"""
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField()
     # https://www.delftstack.com/howto/django/django-phone-number-field/
     phone_regex = RegexValidator(regex=r"^\+?1?\d{8,15}$")
-    phone=models.CharField(validators=[phone_regex], max_length=16, unique=True)
+    phone = models.CharField(validators=[phone_regex], max_length=16, unique=True)
+    partnership = models.ForeignKey(Partnership, on_delete=models.SET_NULL, null=True, blank=True)
 
-
-class Partnership(models.model):
-    """A pairing of 2 partners"""
-    name = models.CharField(max_length=100, null=True, blank=True)
-    partner_1 = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
-    partner_2 = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
-
-class Reason(models.model):
+class Reason(models.Model):
     """A message sent from one partner to another"""
     date = models.DateTimeField()
     partnership = models.ForeignKey(Partnership, on_delete=models.CASCADE)
-    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="authored_reason_set"
+    )
     message = models.TextField()
-    favorited_by = models.ManyToManyField(User, on_delete=models.SET_NULL, null=True, blank=True)
+    favorited_by = models.ManyToManyField(User, related_name="favorited_reason_set")
