7996f20ab738d15a7f1479133200266a3a981f22
Andy Yoon <insoo.yoon@proton.me>
2023-08-13 17:52:31 -0700

    Small model edits, add some data!

--- django/reasons/admin.py
+++ django/reasons/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Partnership, User, Reason
 
 # Register your models here.
+admin.site.register(Partnership)
+admin.site.register(User)
+admin.site.register(Reason)
--- django/reasons/models.py
+++ django/reasons/models.py
@@ -6,15 +6,22 @@ class Partnership(models.Model):
     """A pairing of 2 partners"""
     name = models.CharField(max_length=100, null=True, blank=True)
 
+    def __str__(self):
+        return self.name
+
 class User(models.Model):
     """A user of the app, and a partner in a partnership"""
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    email = models.EmailField()
+    preferred_name = models.CharField(max_length=100, null=True, blank=True)
+    email = models.EmailField(unique=True, null=True, blank=True)
     # https://www.delftstack.com/howto/django/django-phone-number-field/
     phone_regex = RegexValidator(regex=r"^\+?1?\d{8,15}$")
-    phone = models.CharField(validators=[phone_regex], max_length=16, unique=True)
+    phone = models.CharField(validators=[phone_regex], max_length=16, unique=True, null=True, blank=True)
     partnership = models.ForeignKey(Partnership, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
 
 class Reason(models.Model):
     """A message sent from one partner to another"""
@@ -24,8 +31,10 @@ class Reason(models.Model):
         User,
         on_delete=models.SET_NULL,
         null=True,
-        blank=True,
         related_name="authored_reason_set"
     )
     message = models.TextField()
-    favorited_by = models.ManyToManyField(User, related_name="favorited_reason_set")
+    favorited_by = models.ManyToManyField(User, related_name="favorited_reason_set", blank=True)
+
+    def __str__(self):
+        return f"{self.date} by {self.author}"
