0c67c932cf1ff3bd1cb544bfe1484ce1a33f5c13
Andy Yoon <insoo.yoon@proton.me>
2023-08-18 19:48:40 -0700

    Configure keys for prod

--- django/reasons_to_love/settings.py
+++ django/reasons_to_love/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+w+d-6%qn9sn!qt+h-tb3=c%9yo8y*3wh#-m9wc-x*=f6c7_2s'
+# SECRET_KEY = 'django-insecure-+w+d-6%qn9sn!qt+h-tb3=c%9yo8y*3wh#-m9wc-x*=f6c7_2s'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
