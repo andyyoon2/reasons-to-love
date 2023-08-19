3c761dda4c47c70e199b520e493de7bdbf8b5d4e
Andy Yoon <insoo.yoon@proton.me>
2023-08-19 03:04:55 +0000

    Add ec2 ip

--- django/reasons_to_love/settings.py
+++ django/reasons_to_love/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["35.91.65.22"]
 
 
 # Application definition
