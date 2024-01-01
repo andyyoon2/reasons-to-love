dd6fb6dfcafb8c062e0885ddeef5b2763cfed959
Andy Yoon <insoo.yoon@proton.me>
2024-01-01 23:26:51 +0000

    Add basic list view

--- backend/reasons/views.py
+++ backend/reasons/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.views import generic
+from .models import User
 
 # Create your views here.
+class UserListView(generic.ListView):
+    model = User
